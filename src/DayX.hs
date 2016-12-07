8511c19f24125ee8c065ad2fe01352ee3e77aa2a
Matthew Farrell <farrellm@alum.mit.edu>
2016-12-07 01:24:25 -0500

    fix EitherT usage

--- src/DayX.hs
+++ src/DayX.hs
@@ -6,7 +6,8 @@ import ClassyPrelude
 
 import Control.Monad.State (State, evalState, get, put, modify)
 import Control.Monad.Trans.Either
-import Data.Attoparsec.Text hiding (take)
+import Data.Attoparsec.Text
+       (Parser, parseOnly, many1, sepBy, char, letter, string, endOfLine)
 import Data.Char (chr,ord)
 import Data.List (transpose)
 import Data.Vector ((//))
@@ -22,6 +23,7 @@ input = readFile "data/dayX.txt"
 
 parser = letter
 
-result1 = runEitherT $ do
-  i <- lift (parseOnly parser <$> input)
-  pure (i)
+result1 =
+  runEitherT $
+  do i <- EitherT (parseOnly parser <$> input)
+     pure (i)
